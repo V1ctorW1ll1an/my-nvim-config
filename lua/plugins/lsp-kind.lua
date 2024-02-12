1c9f00faeb0dac7732b5422ea5e9d538751b6d0f
Victor Willian <victorwillian1012@gmail.com>
2024-02-12 11:00:47 -0300

    first commit

---
+++ lua/plugins/lsp-kind.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "onsails/lspkind.nvim",
+    config = function()
+      local lspkind = require("lspkind")
+      lspkind.init({
+        mode = 'symbol_text',
+        preset = 'codicons',
+        symbol_map = {
+          Text = "󰉿",
+          Method = "󰆧",
+          Function = "󰊕",
+          Constructor = "",
+          Field = "󰜢",
+          Variable = "󰀫",
+          Class = "󰠱",
+          Interface = "",
+          Module = "",
+          Property = "󰜢",
+          Unit = "󰑭",
+          Value = "󰎠",
+          Enum = "",
+          Keyword = "󰌋",
+          Snippet = "",
+          Color = "󰏘",
+          File = "󰈙",
+          Reference = "󰈇",
+          Folder = "󰉋",
+          EnumMember = "",
+          Constant = "󰏿",
+          Struct = "󰙅",
+          Event = "",
+          Operator = "󰆕",
+          TypeParameter = "",
+        }
+      })
+
+
+      require("cmp").setup {
+        formatting = {
+          format = lspkind.cmp_format {
+            maxwidth = 50,
+            ellipsis_char = "...",
+          },
+        },
+      }
+    end
+  } }
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return { {
+  "folke/noice.nvim",
+  enabled = false
+},
+  {
+    "rcarriga/nvim-notify",
+    enabled = false
+  }
+
+
+}
